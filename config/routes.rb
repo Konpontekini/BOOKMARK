202380e1e3f358bc01af8603e64150b22c35ee71
Bhavya Bakhda <bhavbakhda95@gmail.com>
2022-02-19 17:05:29 +0000

    routes after new category model and collection (get :purchased) added under :items do

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :items do
-    member do
-      get :category
+    collection do
+      get :purchased
     end
+  end
+  resources :categories
   end
 
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
