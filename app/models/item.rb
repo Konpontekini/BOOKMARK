343d4f3956be66211efd2c3f6e9b4c5eead73d85
Frazer Gavin <fcgavin27@gmail.com>
2022-02-19 11:04:48 +0000

    Changed the validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
-  validates :name, :price, :description, :category, :item_url, presence: true
+  validates :category, :item_url, presence: true
   validates :item_url, uniqueness: true
-
 end
