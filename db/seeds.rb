a59ce63015c2b3cdc941cb0db51aeee1f3d07c3e
Fang Zhao <fangzhao021@hotmail.com>
2022-02-19 13:08:23 +0000

    changed cloudinary line in homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require "open-uri"
+
+# puts "Cleaning database..."
+# Item.destroy_all
+
+# puts "Creating items..."
+# curtain = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+# bed =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+
+# [ dishoom, pizza_east ].each do |attributes|
+#   item = Item.create!(attributes)
+#   puts "Created #{item.name}"
+# end
+# puts "Finished!"
