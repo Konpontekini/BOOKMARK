c3b534a91c0753078067f1e98d8c5b98e5665329
Bhavya Bakhda <bhavbakhda95@gmail.com>
2022-02-22 21:31:50 +0000

    category controller done (new, create, destroy)

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,22 +10,28 @@
 puts "Cleaning database..."
 Item.destroy_all
 User.destroy_all
+Category.destroy_all
 
 puts "Creating user..."
 user = User.create!(
     first_name: 'fang',
     last_name: 'zhao',
-    email: 'fangzhao021@hotmail.com',
-    password: '11111111'
+    email: 'bookmark@hotmail.com',
+    password: 'password'
   )
 
 puts "user created!"
 
+puts "Creating categories..."
+
+home = Category.new(name:"home")
+
 puts "Creating items..."
-curtain = { user: User.last, name: "net curtain", description: "nice curtain for bedrooms", price: 86.00, purchased: false, category: "home", item_url: "https://www.johnlewis.com/gp-j-baker-poppy-paisley-made-to-measure-curtains-or-roman-blind-ochre/p5660062" }
-bed = { user: User.last, name: "bed", description: "comfortable bed", price: 729.00, purchased: false, category: "home", item_url: "https://www.johnlewis.com/john-lewis-partners-rattan-bed-frame-king-size/p4147243" }
+
+curtain = { user: User.last, name: "net curtain", description: "nice curtain for bedrooms", price: 86.00, purchased: false, category: home, item_url: "https://www.johnlewis.com/gp-j-baker-poppy-paisley-made-to-measure-curtains-or-roman-blind-ochre/p5660062" }
+bed = { user: User.last, name: "bed", description: "comfortable bed", price: 729.00, purchased: false, category: home, item_url: "https://www.johnlewis.com/john-lewis-partners-rattan-bed-frame-king-size/p4147243" }
 # shoe = { user: User.last, category: "fashion", item_url: "https://www.farfetch.com/uk/shopping/women/prada-logo-plaque-detail-panelled-boots-item-16762068.aspx?storeid=11240" }
-shoe = { user: User.last, name: "boot", description: "fashionable boots", category: "fashion", price: 1100.00, purchased: false, item_url: "https://www.farfetch.com/uk/shopping/women/prada-logo-plaque-detail-panelled-boots-item-16762068.aspx?storeid=11240" }
+shoe = { user: User.last, name: "boot", description: "fashionable boots", category: home, price: 1100.00, purchased: false, item_url: "https://www.farfetch.com/uk/shopping/women/prada-logo-plaque-detail-panelled-boots-item-16762068.aspx?storeid=11240" }
 
 [ curtain, bed, shoe ].each do |attributes|
   item = Item.create!(attributes)
