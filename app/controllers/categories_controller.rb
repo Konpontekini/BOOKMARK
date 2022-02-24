61524d1b826002c6e6c5f8a532bffd1af06bc597
Fang Zhao <fangzhao021@hotmail.com>
2022-02-24 22:48:10 +0000

    fixed item creation issue

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,36 +1,35 @@
 class CategoriesController < ApplicationController
-  before_action :find_category, only: [:update, :edit, :destroy]  
+  # before_action :find_category, only: [:update, :edit, :destroy]  
   
   def index
     @categories = Category.all
   end
 
-  def new
-    @category = Category.new
-  end
+  # def new
+  #   @category = Category.new
+  # end
 
-  def create
-    @category = Category.new(category_params)
-    @category.save
-    redirect_to items_path
-  end
+  # def create
+  #   @category = Category.new(category_params)
+  #   @category.save
+  #   redirect_to items_path
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-    if @category.update(category_params)
-      redirect_to categories_path #question about path --- if change category and url of item at the same time?
-      # redirect_to category_path(@category) 
-    else
-      render :index
-    end
-  end
+  # def update
+  #   if @category.update(category_params)
+  #     redirect_to categories_path
+  #   else
+  #     render :index
+  #   end
+  # end
 
-  def destroy
-    @category.destroy
-    redirect_to items_path #question about path --- want to direct to items index page
-  end
+  # def destroy
+  #   @category.destroy
+  #   redirect_to items_path
+  # end
 
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   validates  :item_url, presence: true
-  validates :item_url, uniqueness: true
+  # validates :item_url, uniqueness: true
+  validates :item_url, uniqueness: { message: "item exists already!" }
 end
