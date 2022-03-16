8e09fc169049db3a9175783ad81c32bf30992ff7
Fang Zhao <fangzhao021@hotmail.com>
2022-03-16 16:51:50 +0000

    changed card footer per comments and cleaned up purchase button

--- app/javascript/components/hover.js
+++ app/javascript/components/hover.js
@@ -18,7 +18,7 @@ export function showHover() {
   const itemCard = document.querySelectorAll(".item-card")
   itemCard.forEach(card => {
     // console.dir(card)
-    const detailShow = card.childNodes[3].childNodes[1]
+    const detailShow = card.childNodes[1].childNodes[3]
     card.addEventListener('mouseover', ()=> {
       detailShow.style.display = "block"
     })
