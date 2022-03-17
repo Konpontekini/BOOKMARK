2ede6ef811363e9da6c0dba38828e8d9322173c9
Fang Zhao <fangzhao021@hotmail.com>
2022-03-17 09:50:03 +0000

    made a few final css changes to add and update category

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,5 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   validates  :item_url, presence: true
-  # validates :item_url, uniqueness: true
-  validates :item_url, uniqueness: { message: "item exists already!" }
+  # validates :item_url, uniqueness: { message: "item exists already!" }
 end
